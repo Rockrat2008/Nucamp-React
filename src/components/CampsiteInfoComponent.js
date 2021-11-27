e970d76877bc506dc22d5c4dbd4e52c985ca8579
Michael OBrien <rockrat2008@gmail.com>
2021-11-27 12:13:40 -0800

    Added validation to form for comments in CampsiteInfo component

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -15,6 +15,10 @@ import {
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+
 function RenderCampsite({ campsite }) {
   return (
     <div className="col-md-5 m-1">
@@ -117,6 +121,22 @@ class CommentForm extends Component {
                   name="author"
                   placeholder="Your Name"
                   className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  component="div"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be at least 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
                 />
               </div>
               <div className="form-group">
@@ -137,6 +157,7 @@ class CommentForm extends Component {
           </ModalBody>
         </Modal>
         <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil fa-lg" />
           Submit Comment
         </Button>
       </React.Fragment>
